d0f031e3b4d57fb593e5db0adf07dd204920e12f
Joel Lacey <joellaceybusiness@gmail.com>
2019-11-19 16:19:49 -0700

    Update round and turn classes to cooperatively handle turn behavior

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,24 @@
 class Round {
   constructor(deck) {
-    this.currentCard = deck ? deck.cards[0] : [];
+    this.cards = deck ? deck.cards : [];
+    this.currentCard = this.cards[0];
+    this.turn = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(turn) {
+    this.turn++;
+    this.currentCard = this.cards[this.turn];
+    if(turn.evaluateGuess()) {
+      return turn.giveFeedback();
+    } else {
+      this.incorrectGuesses.push(turn.guess);
+      return turn.giveFeedback();
+    }
   }
 }
 
--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,23 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    return (this.guess === this.card.correctAnswer);
+  }
+
+  giveFeedback() {
+    const answer = this.evaluateGuess();
+    return (answer ? 'correct!' : 'incorrect!');
+  }
 }
 
 module.exports = Turn;
