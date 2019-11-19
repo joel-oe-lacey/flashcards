d2931e251c0c37492f1b6714c6a644d27aa11cc1
Joel Lacey <joellaceybusiness@gmail.com>
2019-11-19 14:55:34 -0700

    Build out additional Deck tests and initial Round tests

---
+++ test/Round-test.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should have a currentCard property, which starts as the first card in the Deck', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array');
+    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], 'mutator method');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(deck.returnCurrentCard()).to.deep.equal(card1);
+  });
+
+  
+
+});
