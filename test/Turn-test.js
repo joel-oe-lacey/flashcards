e166495178dbaecd78e73f530e75540a6fa797fc
Joel Lacey <joellaceybusiness@gmail.com>
2019-11-18 17:18:21 -0700

    Build initial turn structure to pass tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,26 +1,27 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Turn');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should store user guess', function() {
+  it('should store user guess', function() {
     const turn = new Turn('array');
     expect(turn.guess).to.equal('array');
   });
 
-  it.skip('should also store card associated with guess', function() {
+  it('should also store card associated with guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('array', card);
     expect(turn.card).to.deep.equal({ id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
