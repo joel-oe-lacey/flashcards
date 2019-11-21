38d070359dfeea879c770052036e616b5817191b
Joel Lacey <joellaceybusiness@gmail.com>
2019-11-21 15:44:28 -0700

    Add round timer

--- src/Round.js
+++ src/Round.js
@@ -7,6 +7,7 @@ class Round {
     this.turn = 0;
     this.incorrectGuesses = [];
     this.incorrectCards = [];
+    this.startTime = Date.now();
   }
 
   returnCurrentCard() {
@@ -31,6 +32,11 @@ class Round {
     return (((this.turn - this.incorrectGuesses.length) / this.turn) * 100);
   }
 
+  calculateTime() {
+    const gameEndTime = Date.now();
+    return Math.floor((gameEndTime - this.startTime) / 1000);
+  }
+
   incorrectRound() {
     this.cards = this.incorrectCards;
     this.incorrectGuesses = [];
@@ -45,7 +51,10 @@ class Round {
         return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly! You need 90% to pass, please try incorrect answers again.`;
         break;
       default:
-        return `** Round over! ** You passed with ${this.calculatePercentCorrect()}% of the questions answered correctly!`;
+        const time = this.calculateTime();
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `** Round over! ** You passed with ${this.calculatePercentCorrect()}% of the questions answered correctly! You finished in ${minutes} minutes and ${seconds} seconds`;
         break;
     }
   }
